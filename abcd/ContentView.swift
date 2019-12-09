983173c0c07bacf6c6721069ce569b7ab01b7501
David Gunawan <david_gunawan_@hotmail.com>
2019-12-09 17:40:12 +0700

    Initial Commit

---
+++ abcd/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  abcd
+//
+//  Created by David Gunawan on 09/12/19.
+//  Copyright © 2019 David Gunawan. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
